bf9501a563ab105fb3266b57b431136ddbfed504
e2e AD <e2etestacceleratedata-hue@users.noreply.github.com>
2026-04-17 13:51:29 +0700

    feat(models): update 2 files

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,17 @@
+{{ config(materialized='table') }}
+
+with source as (
+    select * from {{ source('raw', 'customers') }}
+),
+
+renamed as (
+    select
+        customer_id,
+        trim(name) as customer_name,
+        lower(trim(email)) as email,
+        signup_date
+
+    from source
+)
+
+select * from renamed
